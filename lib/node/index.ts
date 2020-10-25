45b8feff16b3ffc41f9391fabb7e6ad114c73ebf
juliocpmelo <julio.melo@imd.ufrn.br>
2020-10-25 11:14:08 -0300

    Missing from last commit

--- lib/node/index.ts
+++ lib/node/index.ts
@@ -1,5 +1,8 @@
+import { Node_Options } from './node-options';
 import { runNodeFile } from './run-file';
 import { runNodeSourceCode } from './run-source';
+
+export { Node_Options };
 
 const node = {
     runFile: runNodeFile,
