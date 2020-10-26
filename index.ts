a961888eb49abb652e2fc75d6a0fe5cc4658885c
juliocpmelo <julio.melo@imd.ufrn.br>
2020-10-26 12:59:03 -0300

    Adding new types to the exports

--- index.ts
+++ index.ts
@@ -1,11 +1,18 @@
 import './lib/init';
-import c from './lib/c';
-import cpp from './lib/cpp';
-import python from './lib/python';
-import node from './lib/node';
-import java from './lib/java';
-import { errorResultCallback, Options, LanguageExtMap, LanguageNames, Result } from './lib/types';
+import { c, C_Options } from './lib/c';
+import { cpp, CPP_Options } from './lib/cpp';
+import { python, Python_Options } from './lib/python';
+import { node, Node_Options } from './lib/node';
+import { java, Java_Options } from './lib/java';
+import { errorResultCallback, LanguageExtMap, LanguageNames, Result } from './lib/types';
+
+
+export { c, C_Options };
+export { cpp, CPP_Options};
+export { python, Python_Options};
+export { node, Node_Options };
+export { java, Java_Options };
+export { errorResultCallback, LanguageExtMap, LanguageNames, Result };
 
 const compileRun = { c, cpp, python, node, java };
-export { c, cpp, python, node, java, errorResultCallback, Options, LanguageExtMap, LanguageNames, Result };
 export default compileRun;
